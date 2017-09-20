5d736e5b2e6d52025a5e6312083bb692a6074392
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-20 16:11:32 +0200

    no message

---
+++ src/main/java/hello/Message.java
@@ -0,0 +1,39 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String message;
+
+    public Message(){
+        this.message= "Default Message";
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
