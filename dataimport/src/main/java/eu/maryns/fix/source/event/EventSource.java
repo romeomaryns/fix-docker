05bdd06a880c3f914a416907c40094054400cede
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-22 16:24:39 +0200

    no message

--- dataimport/src/main/java/eu/maryns/fix/source/event/EventSource.java
+++ dataimport/src/main/java/eu/maryns/fix/source/event/EventSource.java
@@ -5,23 +5,26 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @EnableBinding(Source.class)
-@EnableConfigurationProperties(EventSourceOptionsMetadata.class)
 public class EventSource {
 
-    @Autowired
-    private EventSourceOptionsMetadata options;
-
+    @Bean
     @InboundChannelAdapter(value = Source.OUTPUT)
-    public String eventMessageSource() {
-        return new SimpleDateFormat(this.options.getFormat()).format(new Date());
+    public MessageSource<String> timerMessageSource() {
+        return () -> new GenericMessage<>(new SimpleDateFormat().format(new Date()));
     }
-
 }
 
