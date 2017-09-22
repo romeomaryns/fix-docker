54f99506b7a3ff8a18178bf9e640dd7027cd6fb5
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-22 21:06:20 +0200

    no message

--- dataimport/src/main/java/eu/maryns/fix/source/event/Application.java
+++ dataimport/src/main/java/eu/maryns/fix/source/event/Application.java
@@ -2,7 +2,9 @@ package eu.maryns.fix.source.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws InterruptedException {
