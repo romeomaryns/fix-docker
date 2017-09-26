eae1d286fce2a3686fc4d39e425da1c692940e35
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-26 15:17:49 +0200

    no message

--- data-import/src/main/java/eu/maryns/fix/source/event/EventSource.java
+++ data-import/src/main/java/eu/maryns/fix/source/event/EventSource.java
@@ -1,6 +1,7 @@
 package eu.maryns.fix.source.event;
 
 import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -20,6 +21,9 @@ import java.util.Date;
 
 @EnableBinding(Source.class)
 public class EventSource {
+
+    public EventSource(RabbitTemplate rabbitTemplate, TopicExchange senderTopicExchange) {
+    }
 
     @Bean
     @InboundChannelAdapter(value = Source.OUTPUT)
--- data-log/src/main/java/eu/maryns/fix/sink/log/Application.java
+++ data-log/src/main/java/eu/maryns/fix/sink/log/Application.java
@@ -2,8 +2,10 @@ package eu.maryns.fix.sink.log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
