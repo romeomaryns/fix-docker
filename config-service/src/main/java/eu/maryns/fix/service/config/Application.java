7cbdebb9c99467ff1aa96139f22370e304bf2d16
Romeo Maryns <romeo.maryns@gmail.com>
2017-09-26 11:18:11 +0200

    no message

--- config-service/src/main/java/eu/maryns/fix/service/config/Application.java
+++ config-service/src/main/java/eu/maryns/fix/service/config/Application.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
